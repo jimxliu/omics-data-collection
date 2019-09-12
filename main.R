a61586d11d92c5135849bede91de0eab67d7a492
Liu <xl6v8@umsystem.edu>
2019-09-12 17:43:23 -0500

    pakcage setup

--- main.R
+++ main.R
@@ -1,4 +1,24 @@
+
+if(!requireNamespace("BiocManager", quitely = TRUE)){ 
+   install.packages("BiocManager")
+}
+
+if(!requireNamespace("GEoquery", quietly = TRUE)) {
+   BiocManager::install("GEOquery")
+}
+
+if(!requireNamespace("rentrez", quietly = TRUE)){
+   install.packages("rentrez")
+}
+
+if(!require("hash")) {
+   install.packages("hash")
+}
+
+
 library(rentrez)
 library(GEOquery)
 library(hash)
-library(stringr)
+# library(stringr)
+
+
