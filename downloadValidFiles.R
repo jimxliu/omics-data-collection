c6b317255515691635bfe3ff7cdb3c1bbca8ed63
jimxliu <xinzhou.jim.liu@gmail.com>
2019-09-12 22:53:06 -0500

    update

--- downloadValidFiles.R
+++ downloadValidFiles.R
@@ -1,43 +1,4 @@
-library(rentrez)
 library(GEOquery)
-library(hash)
-
-# Obtain a list of all GEO series (GSE) associated with Maize and Gene expression profiling.
-organism <- "Zea mays [Organism]"
-datatype <- "expression profiling by high throughput sequencing [DataSet Type]"
-suppfiles <- "(CSV [Supplementary Files])"
-
-query <- paste(c(organism, datatype, suppfiles), collapse = " AND ")
-
-res <- entrez_search(db='gds', term=query, use_history=TRUE)
-res$count
-# Get number of GSE records
-# n_records = res$count
-n_records = 20
-res <- entrez_search(db='gds', term=query ,retmax=n_records, use_history=TRUE)
-summary(res)
-
-# Summary of each record by its record id.
-recsum <- entrez_summary(db='gds', id=res$ids)
-recsum
-
-# Extract the accession id and list of samples (GSM) of each GSE
-gse_list <- extract_from_esummary(recsum, c('gse','suppfile'))
-gse_list
-
-gse_list["suppfile",1]
-
-
-# gse <- "GSE129683" # no valid file
-gse <- "GSE111250"
-filePaths <- getGEOSuppFiles(gse, fetch_files = FALSE)
-filePaths
-
-?getGEOSuppFiles
-colnames(filePaths)
-
-downloadValidFiles("GSE111250")
-
 
 
 # Function: download supp csv files for a GSE that follow certain naming patterns
@@ -47,7 +8,7 @@ downloadValidFiles <- function(gse) {
    files <-  getGEOSuppFiles(gse, fetch_files = FALSE)
    files$fileType <- sapply(files$fname, getFileType)
    for(row in 1:nrow(files)) {
-      fileType <- as.character(files[row, "fileType"])
+      fileType <- files[row, "fileType"]
       if(!is.na(fileType)){
          url <- as.character(files[row, "url"])
          fname <- as.character(files[row, "fname"])
@@ -97,8 +58,7 @@ getFileType<- function(fname){
          # print(paste(fname, "raw")) 
          return("raw")
       }
-
-      return()
    } 
+   return()
 }
 
--- main.R
+++ main.R
@@ -15,13 +15,20 @@ if(!requireNamespace("hash", quietly = TRUE)) {
    install.packages("hash")
 }
 
-library(GEOqsadfuery, logical.return = TRUE, quietly = TRUE)
-
+if(!requireNamespace("stringr", quietly = TRUE)) {
+   install.packages("stringr")
+}
 
 library(rentrez)
 library(GEOquery)
 library(hash)
-# library(stringr)
+library(stringr)
+
+source("./isSingleCell.R")
+source("./getGenomeVersion.R")
+source("./getPMID.R")
+source("./downloadValidFiles.R")
+
 
 # Make a query
 organism <- "Zea mays [Organism]"
@@ -41,4 +48,23 @@ recsum <- entrez_summary(db='gds', id=res$ids)
 # Extract the accession id and list of samples (GSM) of each GSE
 gse_list <- extract_from_esummary(recsum, c('gse'))
 
+
+for(num in gse_list[7:7]){
+   gse <- paste0("GSE",num)
+   
+   eList <- getGEO(gse)
+   eData <- eList[[1]]
+   pd <- pData(eData)
+   # dname <- paste0("./data/",gse)
+   # dir.create(dname, recursive = TRUE)
+   
+   version <- getGenomeVersion(pd) 
+   if(is.na(version)) {
+      next
+   }
+   if(isSingleCell(pd)){
+      next
+   }
+   downloadValidFiles(gse)
+}
 
