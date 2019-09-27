5b5271318ded09d10421671e4bce42c7566a0ab6
jimxliu <xinzhou.jim.liu@gmail.com>
2019-09-27 14:10:13 -0500

    update to include different organisms

--- statsForDifferentOrganisms.R
+++ statsForDifferentOrganisms.R
@@ -1,49 +1,58 @@
 library(rentrez)
 source("./utilities.R")
+source("./getGenomeVersion.R")
 
-organisms <- c("Zea mays", "Saccharomyces cerevisiae", "Homo sapiens", "Mus musculus")
+# organisms <- c("Zea mays", "Saccharomyces cerevisiae", "Homo sapiens", "Mus musculus")
+organisms <- c("Homo sapiens", "Mus musculus")
 
 
-organism <- "Zea mays"
+organism <- "Saccharomyces cerevisiae"
 
-results <- getHTSeqResultsByOrganism(organism, pub_date = "1000/01/01-present")
 
-results$ids
+stats <- c()
 
-count <- 0
-for(id in results$ids){
-   gse <- parseIdToAccession(id)
-   print(sprintf("Processing %s", gse))
-   
-   eList <- tryCatch({
-      getGEO(gse)
-   }, error = function(e) {
-      print("Cannot fetch data, SKIP!")
-      NULL
-   })
-   # if there is error fetching the data or has multiple organisms
-   if(is.null(eList) || length(eList) > 1) {
-      next
-   }
-   pd <- pData(eList[[1]])
-   if(organism == "Zea mays"){
-      version <- getGenomeVersion(pd)
-      if(is.null(version)) {
-         next
-      }
-   }
-   if(isSingleCell(pd)){
-      next
-   }
-
-   # has valid data files?
-   files <- getValidFiles(gse = gse, organism = organism)
-   if(!is.null(files)){
-      count <- count + 1
-   }
+for(organism in organisms){
+   start_time <- proc.time()
+   results <- getHTSeqResultsByOrganism(organism, pub_date = "2019/01/01-present")
+   print(organism)
+   print(results$count)
+   # count <- 0
+   # for(id in results$ids){
+   #    gse <- parseIdToAccession(id)
+   #    print(sprintf("Processing %s", gse))
+   #    
+   #    eList <- tryCatch({
+   #       getGEO(gse, getGPL = FALSE)
+   #    }, error = function(e) {
+   #       print("Cannot fetch data, SKIP!")
+   #       NULL
+   #    })
+   #    # if there is error fetching the data or has multiple organisms
+   #    if(is.null(eList) || length(eList) > 1) {
+   #       next
+   #    }
+   #    pd <- pData(eList[[1]])
+   #    if(organism == "Zea mays"){
+   #       version <- getGenomeVersion(pd)
+   #       if(is.null(version)) {
+   #          next
+   #       }
+   #    }
+   #    if(isSingleCell(pd)){
+   #       next
+   #    }
+   # 
+   #    # has valid data files?
+   #    files <- getValidFiles(gse = gse, organism = organism)
+   #    if(!is.null(files)){
+   #       count <- count + 1
+   #    }
+   # }
+   # time <- proc.time() - start_time
+   # stat_string <- sprintf("Organism: %s. Out of %d total results, %d have valid supp files. %nProcessing time: %s", organism, results$count, count, time[[3]])
+   # stats <- c(stats, stat_string)
 }
-count
 
-pData(getGEO("GSE102036")[[1]])
-getGEOSuppFiles("GSE102036", fetch_files = FALSE, makeDirectory = FALSE)
-?getGEOSuppFiles
+for(string in stats){
+   print(string)
+}
