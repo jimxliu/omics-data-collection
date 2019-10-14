e1573903ee18d97ff6475636d4ec75db805d39ab
jimxliu <xinzhou.jim.liu@gmail.com>
2019-10-13 22:22:44 -0700

    process FPKM into data frame

--- code/filter.py
+++ code/filter.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os, re
-from utils import loadData
+from utils import loadData, longestRegion
 
 organism = "Zea mays"
 
@@ -18,16 +18,63 @@ path = "../data/" + organism
 #fname = "../data/zea_mays/GSE83411_annotation_and_FPKM_expression_master_file.csv"
 #fname = "../data/zea_mays/GSE130930_seed_fpkm.txt"
 #fname = "../data/zea_mays/GSE137826_kenwang_FPKM.csv"
-fname = "../data/zea_mays/GSE50191_FPKM.tsv"
+#fname = "../data/zea_mays/GSE50191_FPKM.tsv"
+#fname = "../data/zea_mays/GSE67550_FPKM_table.txt"
 
+def processFPKM(fname):
+   df = loadData(fname)
+   if df is None:
+      return None
+   # For the columns whose dtype is object, or "O", check if the values match gene id pattern
+   gene_id_pattern = "AC\d+\.\d+_FG[0-9a-zA-Z_]+|Zm[0-9a-zA-Z_]+|GRMZM[0-9a-zA-Z_]+"   
+   gene_id_col = None
+   for col in df.columns:
+      if df.dtypes[col] == "O":      
+         series = df.loc[:10, col]
+         if series.str.contains(gene_id_pattern, regex = True).any():
+            # Extract the column data as a Series.
+            gene_id_col = df[col]
+            break
 
-df = loadData(fname)
+   if gene_id_col is not None:
+      # Remove columns with dtype "int64"
+      df = df.select_dtypes(exclude = ["int32", "int64"])
 
-# Select columns whose dtype is object, or "O", indicating string values
-gene_id_pattern = "AC\d+\.\d+_FG[0-9a-zA-Z_]+|Zm[0-9a-zA-Z_]+|GRMZM[0-9a-zA-Z_]+"   
-for col in df.columns:
-   if df.dtypes[col] == "O":      
-      series = df.loc[:10, col]
-      if series.str.contains(gene_id_pattern, regex = True).any():
-         print(col)
+      # Find and extract the most number of consecutive columns with dtype "float64"
+      start, end = longestRegion(df.dtypes, "float64")
+      # data_cols: fpkm data columns
+      data_cols = df.columns.tolist()[start:end+1]
+      # Only eep column names containing FPKM. If none, use the original data_cols
+      fpkm_cols = [col for col in data_cols if re.search("fpkm", col.lower())]
+      data_cols = [col for col in data_cols if "length" not in col.lower()] if not fpkm_cols else fpkm_cols
+      # Rearrange columns
+      new_cols = ["GeneID"] + data_cols
+      df = df[data_cols]
+      df["GeneID"] = gene_id_col
+      df = df[new_cols]
+      # Replace characters that are not [0-9a-zA-Z_] in the column names with "_"
+      df = df.rename(mapper = lambda x: re.sub("[^0-9a-zA-Z_]+", "_", x), axis = "columns")
+      # Remove rows containing NaN
+      df = df.dropna()
+      return df
 
+   return None
+
+#count = 0
+#total = 0
+#for f in os.listdir(path):
+#   fname = os.path.join(path, f)
+#   if os.path.isfile(fname): 
+#      if "fpkm" in f.lower() and "diff" not in f.lower():
+#         total += 1
+#         df = processFPKM(fname) 
+#         #if df is None:
+#         #   count += 1
+#         #   print "Error processing {}".format(fname) 
+#         if df is not None:
+#            print f
+#            print df.head()
+#            print
+         
+df = processFPKM("../data/zea_mays/GSE71723_transcript_fpkms.txt")            
+print df.head()
--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import re
 
+__all__ = ["loadData", "longestRegion"]
+
 def loadData(fname):
    df = None
    if re.search("\.csv$", fname):
@@ -34,6 +36,43 @@ def loadData(fname):
          except Exception as e:
             print e
             print "Skipped! Error loading data from {}".format(fname)   
-  
+   else:
+      try:
+         df = pd.read_csv(fname, sep = "\t")
+      except Exception as e:
+         print e
+         try:
+            df = pd.read_csv(fname)
+         except Exception as e:
+            print e
+            print "Skipped! Error loading data from {}".format(fname)  
+
    return df
+
+# param: li, list object
+# param: target, string
+# return: (start, end), denoting the start and end index of the longest region with same objects
+def longestRegion(li, target):
+   start = end = 0
+   max_start = 0
+   max_end = -1
+   for i in range(len(li)):
+      if li[i] != target:
+         start = -1
+      else:
+         if start == -1:
+            start = i
+         end = i
+         if max_end - max_start < end - start:    
+            max_end = end
+            max_start = start
+   return (max_start, max_end)
+
+
+
+
+
+
+
+
 
